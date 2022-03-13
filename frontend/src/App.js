f3b01d4052a89faf6a1eac614c156abeaa6f1029
shreya shrestha <shreya.shrestha1999@gmail.com>
2022-03-13 20:28:01 +0545

    initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
+	return (
+		<>
+			<Header />
+			<main>
+				<h1>Main</h1>
+			</main>
+			<Footer />
+		</>
+	);
+};
+export default App;
